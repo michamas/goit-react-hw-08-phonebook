bb996ddf1e4fac6545d5d58c5380eab067246b9f
Michał Masiulanis <m.masiulanis@gmail.com>
2023-05-21 11:59:08 +0200

    imports cleared out

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,3 @@
-import Contacts from 'components/Contacts/Contacts.jsx';
-import { Filter } from 'components/Filter/Filter.jsx';
-import { Form } from 'components/Form/Form.jsx';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchContacts } from 'redux/operations.js';
-import { selectContacts } from 'redux/selectors.js';
-
 export default function HomePage() {
   return <div>HOME PAGE</div>;
 }
